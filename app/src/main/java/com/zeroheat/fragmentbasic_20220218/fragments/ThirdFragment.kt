1795a7445537b90bd757c7298d0e495fa2e43bbb
zeroheat0930 <dongjunkorea@naver.com>
2022-02-18 16:10:31 +0900

    세번쨰 프레그먼트

---
+++ app/src/main/java/com/zeroheat/fragmentbasic_20220218/fragments/ThirdFragment.kt
@@ -0,0 +1,38 @@
+package com.zeroheat.fragmentbasic_20220218.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.zeroheat.fragmentbasic_20220218.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class ThirdFragment : Fragment() {
+
+//    1. 어떤 xml? - onCreateView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+//    2. 어떤 동작? - onActivityCreated
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnToast.setOnClickListener {
+
+            Toast.makeText(requireContext(), "세번째 프래그먼트", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+
+}
