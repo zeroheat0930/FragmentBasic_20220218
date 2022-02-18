02f4eb40677363cb0c2a90b74e340fd88a76d3e7
zeroheat0930 <dongjunkorea@naver.com>
2022-02-18 15:17:27 +0900

    프레그먼트() 상속 + xml 끌어와서 연동

--- app/src/main/java/com/zeroheat/fragmentbasic_20220218/fragments/MyFirstFragment.kt
+++ app/src/main/java/com/zeroheat/fragmentbasic_20220218/fragments/MyFirstFragment.kt
@@ -1,4 +1,20 @@
 package com.zeroheat.fragmentbasic_20220218.fragments
 
-class MyFirstFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.zeroheat.fragmentbasic_20220218.R
+
+class MyFirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_my_first, container, false)
+    }
+
 }
